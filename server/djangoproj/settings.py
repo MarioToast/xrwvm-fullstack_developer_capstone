7924c6522aa3aa404f932fdb581e762084fbef74
MarioToast <70570052+MarioToast@users.noreply.github.com>
2026-04-28 08:43:12 +0200

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -29,12 +29,12 @@ SECRET_KEY =\
 DEBUG = True
 
 ALLOWED_HOSTS = [
-	'localhost',
-	"https://mariokong-8000.theiadockernext-0-labs-prod"
-	"-theiak8s-4-tor01.proxy.cognitiveclass.ai"]
+    'localhost',
+    "https://mariokong-8000.theiadockernext-0-labs-prod"
+    "-theiak8s-4-tor01.proxy.cognitiveclass.ai"]
 CSRF_TRUSTED_ORIGINS = [
-	"https://mariokong-8000.theiadockernext-0-labs-prod"
-	"-theiak8s-4-tor01.proxy.cognitiveclass.ai"]
+    "https://mariokong-8000.theiadockernext-0-labs-prod"
+    "-theiak8s-4-tor01.proxy.cognitiveclass.ai"]
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
@@ -100,7 +100,7 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME':
         "django.contrib.auth.password_validation"
-		".UserAttributeSimilarityValidator",
+        ".UserAttributeSimilarityValidator",
     },
     {
         'NAME':
@@ -147,5 +147,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'frontend/static'),
     os.path.join(BASE_DIR, 'frontend/build'),
-	os.path.join(BASE_DIR, 'frontend/build/static'),
+    os.path.join(BASE_DIR, 'frontend/build/static'),
 ]
