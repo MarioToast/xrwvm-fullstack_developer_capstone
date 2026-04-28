2340632a957020f3d32bfe8f543cf535782e9e8c
MarioToast <70570052+MarioToast@users.noreply.github.com>
2026-04-28 16:12:13 +0200

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -109,25 +109,30 @@ def get_dealer_details(request, dealer_id):
     else:
         return JsonResponse({"status":400,"message":"Bad Request"})
 
+
 # Create a `add_review` view to submit a review
 def add_review(request):
     if(request.user.is_anonymous == False):
         data = json.loads(request.body)
         try:
-            response = post_review(data)
-            return JsonResponse({"status":200})
-        except:
-            return JsonResponse({"status":401,"message":"Error in posting review"})
+			response = post_review(data)
+            return JsonResponse({"status": 200})
+		except Exception as e:
+            return JsonResponse({"status": 401,
+								 "message":"Error in posting review"})
     else:
-        return JsonResponse({"status":403,"message":"Unauthorized"})
+        return JsonResponse({"status": 403,
+							 "message": "Unauthorized"})
+
 
 def get_cars(request):
     count = CarMake.objects.filter().count()
     print(count)
-    if(count == 0):
+    if (count == 0):
         initiate()
     car_models = CarModel.objects.select_related('car_make')
     cars = []
     for car_model in car_models:
-        cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
+        cars.append({"CarModel": car_model.name,
+					 "CarMake": car_model.car_make.name})
     return JsonResponse({"CarModels":cars})
